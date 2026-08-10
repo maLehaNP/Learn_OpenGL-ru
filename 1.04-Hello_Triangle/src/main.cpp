a27baa18e2b6f460f36f32323ab872477cb3c3bd
maLeha <maleha-7@internet.ru>
2026-08-10 22:17:38 +0400

    Добавил проверку ошибок, вывод инфы об Opengl, вывод обоих фигур, MSAA и счетчик кадров

--- 1.04-Hello_Triangle/src/main.cpp
+++ 1.04-Hello_Triangle/src/main.cpp
@@ -6,16 +6,22 @@
 
 void key_callback(GLFWwindow *window, int key, int scancode, int action,
                   int mode);
+void error_callback_glfw(int error, const char* description);
 
 int main(int argc, char *argv[]) {
-  glfwInit();
+  if (!glfwInit()) {
+    fprintf( stderr, "ERROR: could not start GLFW3.\n" );
+    return -1;
+  }
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
+  glfwWindowHint(GLFW_SAMPLES, 8);  // MSAA
+
   // Объект окна
-  GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+  GLFWwindow *window = glfwCreateWindow(800, 600, "Hello Triangle", nullptr, nullptr);
   if (window == nullptr) {
     std::cout << "Ошибка при создании окна GLFW" << std::endl;
     glfwTerminate();
@@ -30,6 +36,11 @@ int main(int argc, char *argv[]) {
     return -1;
   }
 
+  // (https://antongerdelan.net/opengl/hellotriangle.html)
+  // Try to call some OpenGL functions, and print some more version info.
+  printf( "Renderer: %s.\n", glGetString( GL_RENDERER ) );
+  printf( "OpenGL version supported %s.\n", glGetString( GL_VERSION ) );
+
   // Viewport
   int width, height;
   glfwGetFramebufferSize(window, &width, &height);
@@ -38,10 +49,16 @@ int main(int argc, char *argv[]) {
   // Установка колбэка ввода
   glfwSetKeyCallback(window, key_callback);
 
+  // Register the error callback function that we wrote earlier.
+  glfwSetErrorCallback(error_callback_glfw);
+
 
   // Вершины треугольника
-  GLfloat vertices[] = {-0.5f, -0.5f, 0.0f, 0.5f, -0.5f,
-                        0.0f,  0.0f,  0.5f, 0.0f};
+  GLfloat vertices[] = {
+    -1.0f, -0.5f, 0.0f,
+     0.0f, -0.5f, 0.0f,
+    -0.5f,  0.5f, 0.0f
+  };
 
   // Vertex Buffer Object
   //---------------------
@@ -138,10 +155,10 @@ int main(int argc, char *argv[]) {
 
   // Прямоугольник
   GLfloat vertices2[] = {
-     0.5f,  0.5f, 0.0f,  // Верхний правый угол
-     0.5f, -0.5f, 0.0f,  // Нижний правый угол
-    -0.5f, -0.5f, 0.0f,  // Нижний левый угол
-    -0.5f,  0.5f, 0.0f   // Верхний левый угол
+     1.0f,  0.5f, 0.0f,  // Верхний правый угол
+     1.0f, -0.5f, 0.0f,  // Нижний правый угол
+     0.0f, -0.5f, 0.0f,  // Нижний левый угол
+     0.0f,  0.5f, 0.0f   // Верхний левый угол
   };
   GLuint indices[] = {  // Помните, что мы начинаем с 0!
     0, 1, 3,   // Первый треугольник
@@ -156,10 +173,13 @@ int main(int argc, char *argv[]) {
   glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
   //----------------------------------------------------------------------------
 
+  GLuint VBO2;
+  glGenBuffers(1, &VBO2);
+
   GLuint VAO2;
   glGenVertexArrays(1, &VAO2);
   glBindVertexArray(VAO2);
-      glBindBuffer(GL_ARRAY_BUFFER, VBO);
+      glBindBuffer(GL_ARRAY_BUFFER, VBO2);
       glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
       glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
       glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
@@ -171,8 +191,28 @@ int main(int argc, char *argv[]) {
   // Отрисовка только линий треугольников
   //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
+  // Frame Rate Counter
+  double prev_s = glfwGetTime();  // Set the initial 'previous time'.
+  double title_countdown_s = 0.1;
+
   // Игровой цикл
   while (!glfwWindowShouldClose(window)) {
+    double curr_s     = glfwGetTime();   // Get the current time.
+	  double elapsed_s  = curr_s - prev_s; // Work out the time elapsed over the last frame.
+	  prev_s            = curr_s;          // Set the 'previous time' for the next frame to use.
+
+    // Print the FPS, but not every frame, so it doesn't flicker too much.
+	  title_countdown_s -= elapsed_s;
+	  if ( title_countdown_s <= 0.0 && elapsed_s > 0.0 ) {
+	  	double fps        = 1.0 / elapsed_s;
+
+	  	// Create a string and put the FPS as the window title.
+	  	char tmp[256];
+	  	sprintf( tmp, "FPS %.2lf", fps );
+	  	glfwSetWindowTitle(window, tmp );
+	  	title_countdown_s = 0.1;
+	  }
+
     glfwPollEvents();
 
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
@@ -181,13 +221,13 @@ int main(int argc, char *argv[]) {
     glUseProgram(shaderProgram);
 
     // Отрисовываем треугольник
-    //glBindVertexArray(VAO);
-    //glDrawArrays(GL_TRIANGLES, 0, 3);
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glBindVertexArray(0);
 
     // Прямоугольник
     glBindVertexArray(VAO2);
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-
     glBindVertexArray(0);
 
     glfwSwapBuffers(window);
@@ -202,5 +242,9 @@ void key_callback(GLFWwindow *window, int key, int scancode, int action, int mod
   // Закрытие при нажатии ESC
   if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
     glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
+void error_callback_glfw(int error, const char* description) {
+  fprintf( stderr, "GLFW ERROR: code %i msg: %s.\n", error, description );
 }
 
