e58b41279be9f0ba3d88c5cb27802c4fc7d3a199
maLeha <maleha-7@internet.ru>
2026-08-10 16:47:29 +0400

    init

---
+++ 1.1-Window/main.cpp
@@ -0,0 +1,14 @@
+/*
+ * https://habr.com/ru/articles/310790/
+ */
+
+
+//#define GLEW_STATIC
+#include <GL/glew.h>  // Обязательно включается до GLFW
+#include <GLFW/glfw3.h>
+
+
+int main (int argc, char *argv[]) {
+  return 0;
+}
+
