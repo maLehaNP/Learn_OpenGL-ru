b252ca4d65831ecff3886260d43029799a736e9b
maLeha <maleha-7@internet.ru>
2026-08-10 20:56:47 +0400

    Закончил 1.04

--- 1.04-Hello_Triangle/main.cpp
+++ 1.04-Hello_Triangle/main.cpp
@@ -3,6 +3,7 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+
 void key_callback(GLFWwindow *window, int key, int scancode, int action,
                   int mode);
 
@@ -14,8 +15,7 @@ int main(int argc, char *argv[]) {
   glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
   // Объект окна
-  GLFWwindow *window =
-      glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+  GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
   if (window == nullptr) {
     std::cout << "Ошибка при создании окна GLFW" << std::endl;
     glfwTerminate();
@@ -38,6 +38,7 @@ int main(int argc, char *argv[]) {
   // Установка колбэка ввода
   glfwSetKeyCallback(window, key_callback);
 
+
   // Вершины треугольника
   GLfloat vertices[] = {-0.5f, -0.5f, 0.0f, 0.5f, -0.5f,
                         0.0f,  0.0f,  0.5f, 0.0f};
@@ -54,7 +55,7 @@ int main(int argc, char *argv[]) {
   //---------------------
 
   // Шейдеры
-  //-------
+  //----------------------------------------------------------------------------
   // Исходный код GLSL
   const char *vertexShaderSource =
       "#version 330 core\n"
@@ -80,12 +81,13 @@ int main(int argc, char *argv[]) {
               << infoLog << std::endl;
   }
 
-  const char *fragmentShaderSource = "#version 330 core\n"
-                                     "out vec4 color;\n"
-                                     "void main()\n"
-                                     "{\n"
-                                     "  color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-                                     "}\0";
+  const char *fragmentShaderSource =
+      "#version 330 core\n"
+      "out vec4 color;\n"
+      "void main()\n"
+      "{\n"
+      "  color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+      "}\0";
   GLuint fragmentShader;
   fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
   glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
@@ -96,7 +98,7 @@ int main(int argc, char *argv[]) {
     std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n"
               << infoLog << std::endl;
   }
-  //-------
+  //----------------------------------------------------------------------------
 
   // Шейдерная программа (Shader Program)
   //-------------------------------------
@@ -128,11 +130,46 @@ int main(int argc, char *argv[]) {
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat),
-                        (GLvoid *)0);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid *)0);
   glEnableVertexAttribArray(0);
   glBindVertexArray(0);
   //--------------------
+
+
+  // Прямоугольник
+  GLfloat vertices2[] = {
+     0.5f,  0.5f, 0.0f,  // Верхний правый угол
+     0.5f, -0.5f, 0.0f,  // Нижний правый угол
+    -0.5f, -0.5f, 0.0f,  // Нижний левый угол
+    -0.5f,  0.5f, 0.0f   // Верхний левый угол
+  };
+  GLuint indices[] = {  // Помните, что мы начинаем с 0!
+    0, 1, 3,   // Первый треугольник
+    1, 2, 3    // Второй треугольник
+  };
+
+  // Element Buffer Object
+  //----------------------------------------------------------------------------
+  GLuint EBO;
+  glGenBuffers(1, &EBO);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+  //----------------------------------------------------------------------------
+
+  GLuint VAO2;
+  glGenVertexArrays(1, &VAO2);
+  glBindVertexArray(VAO2);
+      glBindBuffer(GL_ARRAY_BUFFER, VBO);
+      glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
+      glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+      glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+      glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+      glEnableVertexAttribArray(0);
+  glBindVertexArray(0);
+
+
+  // Отрисовка только линий треугольников
+  //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
   // Игровой цикл
   while (!glfwWindowShouldClose(window)) {
@@ -141,10 +178,16 @@ int main(int argc, char *argv[]) {
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    // Отрисовываем треугольник
     glUseProgram(shaderProgram);
-    glBindVertexArray(VAO);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    // Отрисовываем треугольник
+    //glBindVertexArray(VAO);
+    //glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    // Прямоугольник
+    glBindVertexArray(VAO2);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
     glBindVertexArray(0);
 
     glfwSwapBuffers(window);
@@ -155,9 +198,9 @@ int main(int argc, char *argv[]) {
   return 0;
 }
 
-void key_callback(GLFWwindow *window, int key, int scancode, int action,
-                  int mode) {
+void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode) {
   // Закрытие при нажатии ESC
   if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
     glfwSetWindowShouldClose(window, GL_TRUE);
 }
+
